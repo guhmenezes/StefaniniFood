ad608c8f8851fa1d89b5bde0efe19f4c64549fbf
Gustavo Henrique Da Silva De Menezes <ghmenezes@stefanini.com>
2023-01-17 23:34:37 -0300

    fix prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  API: 'https://rest-api-production-14e8.up.railway.app/',
+  token: 'token',
+  API_LOC: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
+  API_KEY: '&key=AIzaSyBaTwxJvzKrvEjj8tDkVzzdciod9IkYI7c'
 };
